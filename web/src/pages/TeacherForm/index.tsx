4f9347f1a0a926735eacce21a92b597da15e54fd
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-18 00:49:06 -0300

    css teacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 
+import warningIcon from "../../assets/images/icons/warning.svg";
+
 import "./styles.css";
 
 export default function TeacherForm() {
@@ -20,6 +22,22 @@ export default function TeacherForm() {
           <Input name="avatar" label="Avatar" />
           <Input name="whatapp" label="WhatsApp" />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preecha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
