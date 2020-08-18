0a1914c14b12dfc5d1e1cecf6aeae2b70caf1d8b
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-17 23:59:09 -0300

    component props descriptions

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -8,6 +8,27 @@ export default function TeacherForm() {
         title="Que incrível que você quer dar aulas"
         description="O primeiro passo é preencher esse formuário de inscrição"
       />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Nome Completo</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">WhatsApp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
