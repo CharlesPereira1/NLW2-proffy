25946d71e54f7c913d700f8331e5e95936213cca
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-19 21:30:07 -0300

    component textarea

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,6 +5,7 @@ import Input from "../../components/Input";
 import warningIcon from "../../assets/images/icons/warning.svg";
 
 import "./styles.css";
+import Textarea from "../../components/Textarea";
 
 export default function TeacherForm() {
   return (
@@ -21,6 +22,7 @@ export default function TeacherForm() {
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
