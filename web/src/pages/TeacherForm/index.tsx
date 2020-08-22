c64d820882793d8746e9b06bfff199f6a1f42479
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-21 23:23:06 -0300

    conectando o backend

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -10,13 +10,12 @@ import warningIcon from "../../assets/images/icons/warning.svg";
 import "./styles.css";
 
 export default function TeacherForm() {
-  const scheduleItems = [
-    { week_day: 0, from: "08:00 AM", to: "4:00 PM" },
-    { week_day: 2, from: "10:00 AM", to: "6:00 PM" },
-  ];
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: "", to: "" },
+  ]);
 
   function addNewScheduleItem() {
-    console.log("cliquei");
+    setScheduleItems([...scheduleItems, { week_day: 0, from: "", to: "" }]);
   }
   return (
     <div id="page-teacher-form" className="container">
