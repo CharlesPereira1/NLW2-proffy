6726e363869a39dc41a5f0ce0e97b4438b811b04
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-24 22:51:03 -0300

    passagem de api para component

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,31 +4,38 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  teacher: string;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+export interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/54192694?s=460&u=a0ac6a9b16621a72fd3bfd6bba0c0081c2259d5b&v=4"
-          alt="Charles Pereira"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Charles Pereira</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de Matemática Avançada.
-        <br />
-        <br />
-        Apaixonado pelos melhores calculos, para mudar as vidas da pessoas.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 59,90</strong>
+          <strong>{teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="whatsapp" />
@@ -37,6 +44,6 @@ function TeacherItem() {
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
