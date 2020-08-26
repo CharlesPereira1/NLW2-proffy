e270466925b1958241c040acab6a0dd7fb3c5387
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-25 23:54:22 -0300

    images, index.d.ts e import de fonts expo google fonts

---
+++ mobile/src/pages/Lading/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#8257e5",
+    flex: 1,
+    justifyContent: "center",
+    padding: 40,
+  },
+
+  banner: {
+    width: "100%",
+    resizeMode: "contain",
+  },
+
+  title: {
+    fontFamily: "Archivo_400Regular",
+    color: "#fff",
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+
+  titleBold: {
+    fontFamily: "Poppins_600SemiBold",
+  },
+});
+
+export default styles;
