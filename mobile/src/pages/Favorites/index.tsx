243e2764b04ebef27e38313a1640891d718df31d
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-28 00:23:38 -0300

    novas pastas

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, ImageBackground, Text } from "react-native";
+
+import styles from "./styles";
+
+function Favorites() {
+  return <View />;
+}
+
+export default Favorites;
