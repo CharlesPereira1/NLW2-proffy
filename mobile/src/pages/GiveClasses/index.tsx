7f5f94d51a5c810aab89d4f42ff1d450ad1d2c9d
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-28 00:16:05 -0300

    styles gieclasses

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,30 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ImageBackground, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+import givaeClassesBgImages from "../../assets/images/give-classes-background.png";
+import styles from "./styles";
 
 function GiveClasses() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={givaeClassesBgImages}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
 }
 
 export default GiveClasses;
