0605af212395d33ca0b081987bd30ae2bbc23681
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-30 21:37:47 -0300

    component exclusivo do RN para, contentContainerStyle

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ImageBackground, Text } from "react-native";
+import { View, ScrollView } from "react-native";
 
 import TeacherItem from "../../components/TeacherItem";
 
@@ -12,7 +12,22 @@ function TeacherList() {
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
 
-      <TeacherItem />
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
