247c2df6b5c43af6b62315e3b27fab5cc7129a10
Charles Pereira <charlesti.ptu@gmail.com>
2020-08-30 21:30:46 -0300

    criando component de TeacherItem

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, ImageBackground, Text } from "react-native";
 
+import TeacherItem from "../../components/TeacherItem";
+
 import PageHeader from "../../components/PageHeader";
 
 import styles from "./styles";
@@ -9,6 +11,8 @@ function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+      <TeacherItem />
     </View>
   );
 }
